59262e34ee498d3b3cad6abf1734b88dc827ad2d
Ainarka <ainarka11@gmail.com>
2024-12-29 19:28:13 +0600

    Add files via upload

---
+++ hw8/app.js
@@ -0,0 +1,48 @@
+function Star(isChecked) {
+  const div = document.createElement("div");
+  div.className = "fa fa-star";
+  if (isChecked) {
+    // div.className += 'checked'
+    div.classList.add("checked");
+  }
+  return div;
+}
+
+const stars = document.querySelector(".stars");
+
+function StarRating(rating = 0) {
+  stars.innerHTML = "";
+  const maxRating = 5;
+  for (let i = 0; i < maxRating; i++) {
+    const star = Star(i < rating);
+    stars.append(star);
+    star.onclick = function () {
+      const starPosition = i + 1;
+      StarRating(starPosition);
+      if (starPosition === maxRating) {
+        blowConfetti();
+      }
+    };
+  }
+}
+
+StarRating(0);
+
+function blowConfetti() {
+  let confetti = new Confetti("container");
+
+  confetti.setCount(75);
+  confetti.setSize(1);
+  confetti.setPower(25);
+  confetti.setFade(false);
+  confetti.destroyTarget(false);
+}
+
+$(document).ready(function () {
+  $(document).snowfall({
+    image: "img/snow3.png",
+    minSize: 10,
+    maxSpeed: 1,
+    maxSize: 15,
+  });
+});
